14c00980d43cb5813aaeefdb6d01388846acaadc
Akmat <pochta.akmat@gmail.com>
2016-06-23 16:28:27 +0600

    Готово меню

---
+++ index.php
@@ -0,0 +1,36 @@
+﻿<html>
+<head>
+	<title>Главный</title>
+	<link rel='stylesheet'href='style.css'>
+</head>
+<body>
+	<div class="roditel">
+		<div class='header'>
+			<img src='logo/logo.png'>
+			<div class='menu'>
+				<ul class='main-menu'>
+					<li><a href='#'>Главный</a>
+					<li><a href='#'>Сотрудники</a>
+					<li><a href='#'>Учебный пособия</a>
+					<li class='opened'><a href='#'>О Кафедре</a>
+				</ul>
+			</div>
+				<div class='search'>
+					<form action='#' method='post'>
+						<input type='text' name='search' size='40' placeholder='Поиск по сайту'>
+						<input type='submit' value='Поиск'>
+					</form>
+				</div>		
+		</div>
+		<div class='content'>
+			<div class='slider'>slider</div>
+			<div class='content_in'>
+				
+			</div>
+		</div>
+		<div class='footer'>
+			footer
+		</div>
+	</div>
+</body>
+</html>
